284a10b4291046cb27e219241505185f39cd2641
Gerixmus <gerixmuss@gmail.com>
2025-08-06 01:55:52 +0200

    Deploy website - based on 8935437633ff23592aef46ecd24813bddcc1a0a2

---
+++ assets/js/5e95c892.0817cf65.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksupergit_docs=self.webpackChunksupergit_docs||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(6540);var u=r(4164),c=r(7559),t=r(5500),a=r(2831),d=r(1656),n=r(4848);function p(e){return(0,n.jsx)(t.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,a.v)(e.route.routes)})})}}}]);
